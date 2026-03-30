f5fe5a7a59067f3d05ed20f93451dc6217668c93
John Rey <adioskid@mail.ru>
2026-03-31 02:01:13 +0400

    Заготовка юнит-тестов

---
+++ src/test/java/ru/app/work/service/ServiceTest.java
@@ -0,0 +1,20 @@
+package ru.app.work.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void executeTest() {
+    }
+}
